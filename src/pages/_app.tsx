503865f56b8bbd4a5f0762971b43fbbb6935abd3
cktsun1031 <cktsun1031@gmail.com>
2022-12-12 19:42:34 +0800

    feat: change eng font to `Spline Sans Mono`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '$styles/default.css';
 import '$lib/analytics';
 
+import { Noto_Sans_HK, Noto_Sans_SC, Spline_Sans_Mono } from '@next/font/google';
+import clsx from 'clsx';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
@@ -11,6 +13,22 @@ import { THEME_KEY } from '$data/constants';
 import ApplicationLayout from '$layouts/application';
 import seoConfig from '$lib/seo-config';
 
+// Google Fonts
+const splineSansMono = Spline_Sans_Mono({
+    subsets: ['latin'],
+    variable: '--spline-sans-mono',
+});
+const notoHK = Noto_Sans_HK({
+    variable: '--noto-hk',
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+});
+const notoSC = Noto_Sans_SC({
+    variable: '--noto-sc',
+    subsets: ['latin'],
+    weight: ['400', '500', '700'],
+});
+
 export default function NextApplcation({ Component, pageProps }: AppProps) {
     return (
         <>
@@ -20,7 +38,10 @@ export default function NextApplcation({ Component, pageProps }: AppProps) {
             </Head>
             <ThemeProvider enableSystem attribute="class" storageKey={THEME_KEY}>
                 <NextNProgress color="#eb7236" height={2} options={{ showSpinner: false }} />
-                <ApplicationLayout>
+                <ApplicationLayout
+                    className={clsx(
+                        `${splineSansMono.variable} ${notoHK.variable} ${notoSC.variable} font-sans`,
+                    )}>
                     <Component {...pageProps} />
                 </ApplicationLayout>
             </ThemeProvider>
