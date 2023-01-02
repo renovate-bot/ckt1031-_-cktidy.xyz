4715fc3424da08196f2298ccf991fa8f1a867b37
cktsun1031 <cktsun1031@gmail.com>
2023-01-02 11:33:48 +0800

    chore: remove 2023 countdown banner

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,7 +1,6 @@
 import { NextSeo } from 'next-seo';
 
 import SettingsDisableSnowFall from '$components/settings/disable-snowfall';
-import SettingsHide2023CountDownBanner from '$components/settings/hide-2023-banner';
 import SettingsThemeMode from '$components/settings/theme-mode';
 
 export default function Settings() {
@@ -12,7 +11,6 @@ export default function Settings() {
                 <h1 className="mb-5 text-3xl font-bold">Settings</h1>
                 <div className="mb-10 flex flex-col gap-5">
                     <SettingsThemeMode />
-                    <SettingsHide2023CountDownBanner />
                     <SettingsDisableSnowFall />
                 </div>
             </div>
