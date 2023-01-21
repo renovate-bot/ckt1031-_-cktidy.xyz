114ceea99d82b60f48057f550b86700c86fc0977
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-01-21 20:03:36 +0800

    chore: bump deps

---
+++ sentry.edge.config.ts
@@ -0,0 +1,9 @@
+import { init } from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN ?? process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    enabled: process.env.NODE_ENV === 'production',
+});
