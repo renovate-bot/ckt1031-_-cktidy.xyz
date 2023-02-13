a8d654b04ad3e6aafeb5cf956554880378533933
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-13 19:48:22 +0800

    refactor: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,29 +9,18 @@ export default function HomePage() {
   return (
     <>
       <NextSeo title={title} description={description} openGraph={{ description, title }} />
-      <div className="mt-5 mb-10 flex flex-col items-start justify-start">
-        <div className="flex h-full flex-row justify-between space-x-10">
-          <div className="mt-2 max-w-xl text-gray-700 dark:text-gray-400">
-            <h1 className="bg-gradient-to-r from-gray-500 to-gray-900 bg-clip-text text-4xl font-bold text-transparent dark:from-gray-200 dark:to-gray-500">
-              cktsun
-            </h1>
-            <p className="mt-3 text-black dark:text-white">
-              A secondary school (Form 5) student in Hong Kong.
-            </p>
-            <div className="mt-3 space-y-1">
-              <p>I love programming and reading.</p>
-              <p>Written this website for sharing what I have found in my life.</p>
-            </div>
-          </div>
-          <Image
-            alt="ckt"
-            src="/media/avatar.png"
-            // Disable image shrinking when screen width is less than 150px
-            className="h-32 w-32 rounded-full"
-            height={150}
-            width={150}
-          />
-        </div>
+      <div className="my-10 flex flex-col items-center justify-center">
+        <Image
+          alt="ckt"
+          src="/media/avatar.png"
+          className="h-32 w-32 rounded-full"
+          height={150}
+          width={150}
+        />
+        <h1 className="mt-4 text-4xl font-bold">Chan Ka Tsun</h1>
+        <p className="mt-2 text-center text-lg font-medium italic text-gray-600 dark:text-gray-300">
+          A secondary school (Form 5) student in Hong Kong.
+        </p>
       </div>
     </>
   );
