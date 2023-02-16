b778c1233e9f776b1565699dac814204de70a880
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-16 16:04:08 +0800

    refactor: revamp about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -44,13 +44,27 @@ export default function AboutPage() {
           title="About Me"
           description="👋 Hey there! I'm cktsun 😁, here's' the information about me and messages which is going to tell you! This is the place where you can find out more about me."
         />
-        <div className="mb-10 w-full space-y-10 text-left">
-          <div className="mt-4 flex flex-row flex-wrap gap-6 text-gray-800 dark:text-gray-300">
+        <div className="mb-10 mt-3 w-full space-y-6 text-left">
+          <p>I am a High School Student from Hong Kong, currently F5, 16 yrs.</p>
+          <p>
+            I love reading, writing, and coding. I enjoy investigating philosophical questions and
+            the intersection between technology and ethics
+          </p>
+          <p>
+            I am passionate about exploring ideas and discussing them with others, and I enjoy
+            sharing my thoughts and knowledge with people online.
+          </p>
+          <p>
+            Additionally, I find programming to be a fascinating way to create and problem-solve,
+            and I love the feeling of accomplishment that comes from building something new and
+            useful with code.
+          </p>
+          <div className="flex flex-row flex-wrap gap-2 text-gray-800 dark:text-gray-300">
             {socialMedia.map(item => (
               <a
                 key={item.name}
                 href={item.link}
-                className="base-border flex flex-row items-center gap-2 rounded border bg-gray-100 p-2 dark:bg-gray-900"
+                className="base-border flex flex-row items-center gap-2 rounded-lg border bg-gray-100 p-2 dark:bg-gray-900"
                 target="_blank"
                 rel="noreferrer"
               >
