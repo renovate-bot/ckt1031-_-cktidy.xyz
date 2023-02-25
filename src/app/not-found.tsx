9da40622f9c2cc18ab3c5343c650c7a13b6478b1
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-25 17:30:27 +0800

    fix: not found page styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
   },
 };
 
-export default function NotFoundPage() {
+export default function NotFound() {
   return (
     <div className="mt-5 flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center md:flex-row">
-        <div className="flex flex-col text-center">
-          <p className="mb-4 text-6xl font-bold">404</p>
-          <p className="text-xl md:text-2xl">The page you are looking for does not exist.</p>
-        </div>
+      <div className="flex flex-col items-center justify-center text-center">
+        <p className="mb-4 text-6xl font-bold">404</p>
+        <p className="text-xl md:text-2xl">
+          Oops! It seems like the page you were searching for does not exist.
+        </p>
       </div>
       <Link href="/">
         <button className="button-normal mt-6" type="button">
