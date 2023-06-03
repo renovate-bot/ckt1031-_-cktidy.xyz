efee6710694a75175c90eb6ca5323b335e1d34d7
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-06-03 16:16:16 +0800

    fix: lint types

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,11 +1,13 @@
-import type { LinkProps } from 'next/link';
+import type { UrlObject } from 'node:url';
 import Link from 'next/link';
 
 import clsx from 'clsx';
 
-interface Props extends LinkProps<HTMLAnchorElement> {
+interface Props {
   className?: string;
+  href: UrlObject | __next_route_internal_types__.RouteImpl<''>;
   children?: React.ReactNode;
+  [x: string]: unknown;
 }
 
 export default function LinkComponent({ href, children, className, ...rest }: Props) {
